ce91a52ec82addb837a8b58285444635221e70ec
Jason Lee <nblueyi55@gmail.com>
2024-04-08 16:56:33 -0500

    revised

--- app.js
+++ app.js
@@ -33,40 +33,65 @@ arraySum(exampleNumbers);
 
 // Exercise 2 Section
 
-let favBook = {};
+//vreate an object
+const favBook = {};
 
 favBook.title = "Family";
 favBook.author = "Pa Chin";
-favBook.pageCount = 300;
-favBook.readCount = 1;
-
-console.log(favBook);
-
-function info (){
+favBook.numCount = 300;
+favBook.readCount = 0;
 
 favBook.info = function () {
    return `${this.title} by ${this.author} is ${this.pageCount} pages has been read ${this.readCount} times`;
-};
-console.log(favBook.info());
 }
+console.log(favBook.info());
 
 // Exercise 3 Section
-let sentence = "The quick down for jumps over the lazy dog";
-
-function reverseWords(sentence) {
-  let result = [];
-
-  let words = sentence.split(" ");
+function reverseWordsInSentence(sentence) {
+ //sentence = "The quick down for jumps over the lazy dog";
+const words = sentence.split(" ");
+console.log(words);
+// Iterate (erpeat a process)
   for (let i = 0; i < words.length; i ++) {
-  let letters = words[i].split("");
+
+  // each words = words[i]
+  const letters = words[i].split("");
+    // reverse
   letters.reverse();
-  let reverseWord = letters.join("");
-  result.push(reverseWord);
+  // update someting that lives outside of the for loops's scope
+  words[i] = letters.join("");   // array to string
+  }
+const result = words.join(" ");
+return result;
 }
-return result.join(" ");
-}
-console.log(reverseWords(sentence));
-console.log(reverseWords("Good morning, john"));
+console.log(reverseWordsInSentence("The quick down for jumps over the lazy dog"));
 
+// Exercise 4 Section
+let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
 
+function parseCSV(csvString) {
+  //strings can .split
+  //arrays can .join()
+    const lines = csvString.split("\n");
+    const headers = lines[0].split(",");
+
+    //create an array to use later. we are going to store the objects in here.
+    const resultArray = [];
+   //iterate means repeat a process
+   for (let i = 0; i < lines.length; i ++) {
+       //split the lines into individual data elements
+       const data = lines[i].split(",");
+       const tempObj = {
+         name: data[0],
+         age: data[1],
+       }
+       resultArray.push(tempObj);
+      }
+       //Ends the function
+       //Returns the code to whereever it was before invoking this function
+       //And sends back the value
+      return resultArray;
+     
+}  
+console.log(parseCSV(csvData));
 
